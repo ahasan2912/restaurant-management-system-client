8f0fa00876240e2ed50ff107be603e04caf7a760
ahasan2912 <habib15-4284@diu.edu.bd>
2024-12-25 10:56:07 +0600

    navbar gooter mainlayout

--- src/pages/AllFood.jsx
+++ src/pages/AllFood.jsx
@@ -13,11 +13,11 @@ const AllFood = () => {
     }, [])
     return (
         <div className='max-w-7xl mx-auto'>
-             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mt-10'>
+            {/*  <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mt-10'>
                 {
                     foods.map(food => <FoodCart key={food._id} food={food}></FoodCart>)
                 }
-            </div>
+            </div> */}
         </div>
     );
 };
